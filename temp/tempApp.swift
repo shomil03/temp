1bf7aacbf8563ed530c18fcc05b07371f41e25aa
shomil03 <shomil02020@gmail.com>
2023-09-01 04:11:11 +0530

    Initial Commit

---
+++ temp/tempApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempApp.swift
+//  temp
+//
+//  Created by Shomil Singh on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct tempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
